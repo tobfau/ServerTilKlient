1f9e345b2cb97eacebe627b5a0eea511e78b656e
Michael Folkmann <michaelfolkmann@michaels-mbp.hhk.dk>
2016-10-12 12:03:00 +0200

    serviceimpl update

--- src/dal/ServiceImpl.java
+++ src/dal/ServiceImpl.java
@@ -1,15 +1,50 @@
 package dal;
 
-import java.sql.Connection;
+import service.Service;
+import shared.StudentDTO;
+import shared.TeacherDTO;
+
+import java.sql.*;
 
 /**
  * Created by emilstepanian on 12/10/2016.
  */
-public class ServiceImpl {
+public class ServiceImpl implements Service {
 
-    Connection dbConnection =null;
+    private static Connection dbConnection =null;
+    private static final String URL = "jdbc:mysql://localhost:3306/mydb";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "repsej";
 
-    int db = 20;
+    public ServiceImpl() {
+        try {
+            dbConnection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+    public void close(){
+        try {
+            dbConnection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public StudentDTO loginStudent(String username, String password) throws SQLException {
+        ResultSet resultSet = null;
+
+        StudentDTO user = null;
+
+        PreparedStatement approveUser = dbConnection.prepareStatement("");
+
+
+    }
+    public TeacherDTO loginTeacher(String username, String password) throws SQLException {
+
+    }
 
 
 }
