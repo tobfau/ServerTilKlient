eec1de7d491358019cce49bac33d021fe8288d3c
emilstepanian <emilstepanian@hotmail.com>
2016-10-12 12:37:56 +0200

    CbsConnector oprettet og der er forsøgt at oprette lectures. Courses fungerer ikke endnu

---
+++ src/logic/CbsConnector.java
@@ -0,0 +1,70 @@
+package logic;
+
+import com.google.gson.Gson;
+import shared.CourseDTO;
+import shared.LectureDTO;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+/**
+ * Created by emilstepanian on 12/10/2016
+ */
+public class CbsConnector {
+    public static void main(String []args) {
+        CbsConnector sc = new CbsConnector();
+        sc.start();
+    }
+    public void start() {
+        try {
+
+
+            Gson gson = new Gson();
+
+            ArrayList<LectureDTO> arrayListLectureDTOs = new ArrayList<LectureDTO>();
+
+            LectureDTO lectureDTO = new LectureDTO();
+            CourseDTO courseDTO = new CourseDTO();
+            for (String string : getLectureDTOArray()) {
+
+                lectureDTO = gson.fromJson(string, lectureDTO.getClass());
+                System.out.println(lectureDTO.toString());
+
+                courseDTO = gson.fromJson(string, courseDTO.getClass());
+                System.out.println(courseDTO.toString());
+
+                arrayListLectureDTOs.add(lectureDTO);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+        }
+    }
+
+    public static ArrayList<String> getLectureDTOArray() throws Exception {
+        ArrayList<String> lectureDTOArray = new ArrayList<String>();
+        StringBuilder result = new StringBuilder();
+        URL url = new URL("https://calendar.cbs.dk/events.php?format=json&groups=BINTO1067U_LA_E16,BINTO2056U_LA_E16,BINTO1051U_LA_E16,BINTO1035U_LA_E16");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+        String line;
+        String lineCommaRemoved;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+            lineCommaRemoved = line.substring(0, line.length()-1);
+            lectureDTOArray.add(lineCommaRemoved);
+        }
+        lectureDTOArray.remove(0);
+        rd.close();
+        return lectureDTOArray;
+    }
+
+}
--- src/shared/LectureDTO.java
+++ src/shared/LectureDTO.java
@@ -1,9 +1,146 @@
+
 package shared;
 
-/**
- * Created by emilstepanian on 12/10/2016.
- */
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("org.jsonschema2pojo")
 public class LectureDTO {
 
-    //hej
+    @SerializedName("activityid")
+    @Expose
+    private String activityid;
+    @SerializedName("eventid")
+    @Expose
+    private String eventid;
+    @SerializedName("type")
+    @Expose
+    private String type;
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("description")
+    @Expose
+    private String description;
+    @SerializedName("start")
+    @Expose
+    private List<String> start = new ArrayList<String>();
+    @SerializedName("end")
+    @Expose
+    private List<String> end = new ArrayList<String>();
+    @SerializedName("location")
+    @Expose
+    private String location;
+
+
+    public LectureDTO() {
+    }
+
+
+    public LectureDTO(String activityid, String eventid, String type, String title, String description, List<String> start, List<String> end, String location) {
+        this.activityid = activityid;
+        this.eventid = eventid;
+        this.type = type;
+        this.title = title;
+        this.description = description;
+        this.start = start;
+        this.end = end;
+        this.location = location;
+    }
+
+
+    public String getActivityid() {
+        return activityid;
+    }
+
+
+    public void setActivityid(String activityid) {
+        this.activityid = activityid;
+    }
+
+
+    public String getEventid() {
+        return eventid;
+    }
+
+
+    public void setid(String eventid) {
+        this.eventid = eventid;
+    }
+
+
+    public String getType() {
+        return type;
+    }
+
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    public List<String> getStart() {
+        return start;
+    }
+
+
+    public void setStart(List<String> start) {
+        this.start = start;
+    }
+
+
+    public List<String> getEnd() {
+        return end;
+    }
+
+
+    public void setEnd(List<String> end) {
+        this.end = end;
+    }
+
+
+    public String getLocation() {
+        return location;
+    }
+
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "\nLectureDTO{" +
+                "\nactivityid='" + activityid + '\'' +
+                ",\n eventid='" + eventid + '\'' +
+                ",\n type='" + type + '\'' +
+                ",\n title='" + title + '\'' +
+                ",\n description='" + description + '\'' +
+                ",\n start=" + start +
+                ",\n end=" + end +
+                ",\n location='" + location + '\'' +
+                '}';
+    }
 }
