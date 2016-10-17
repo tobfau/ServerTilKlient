3cfe857c76d6f1dafbfbef51fe6bfdf07e8fb688
emilstepanian <emilstepanian@hotmail.com>
2016-10-17 10:54:51 +0200

    - Prøver at få config fil til at virke. Hul igennem?

--- src/logic/Config.java
+++ src/logic/Config.java
@@ -2,6 +2,8 @@ package logic;
 
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.stream.JsonReader;
 
 import java.io.FileReader;
@@ -19,17 +21,27 @@ public class Config {
 
         JsonReader jsonReader;
         Gson gson;
-        System.out.println("dsf");
 
 
         try {
             jsonReader = new JsonReader(new FileReader("config.dist.json"));
             gson = new Gson();
-            System.out.println("sdagdf");
-            CBS_API_link = gson.fromJson(jsonReader, (String) CBS_API_link);
 
-            System.out.println("2");
-            System.out.println(CBS_API_link);
+            JsonObject obj = gson.fromJson(jsonReader, JsonObject.class);
+
+            JsonElement element = obj.get("CBS_API_link");
+
+            System.out.println(element.getass);
+
+
+
+            CBS_API_link = element.getAsString();
+
+     //       JsonObject obj = gson.fromJson(jsonReader, JsonObject.class);
+
+
+            //CBS_API_link = gson.fromJson(jsonReader, (String) CBS_API_link);
+
         } catch (Exception e) {
             e.printStackTrace();
 
