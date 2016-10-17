2e476522be24d836fe228a710cb408193ac9fb6d
Michael Folkmann <michaelfolkmann@michaels-mbp.hhk.dk>
2016-10-17 08:47:27 +0200

    Oprydning efter en masse pis.

--- src/dal/ServiceImpl.java
+++ src/dal/ServiceImpl.java
@@ -1,16 +1,14 @@
-/**package dal;
+package dal;
 
-import service.Service;
-import shared.LectureDTO;
-import shared.StudentDTO;
-import shared.TeacherDTO;
+
+import shared.*;
 
 import java.sql.*;
 
-/**
- * Created by emilstepanian on 12/10/2016.
 
-public class ServiceImpl implements Service {
+//Created by emilstepanian on 12/10/2016.
+
+public class ServiceImpl {
 
     private static Connection dbConnection =null;
     private static final String URL = "jdbc:mysql://localhost:3306/mydb";
@@ -34,11 +32,12 @@ public class ServiceImpl implements Service {
         }
     }
 
-    /*
+
 
 
     //Forberedt main-metode, til test af diverse metoder.
 
+/*
     public static void main(String[] args) {
         ServiceImpl d = new ServiceImpl();
         ArrayList<StudentDTO> arr = d.getStudents();
@@ -50,10 +49,11 @@ public class ServiceImpl implements Service {
     }
 
 
+*/
 
 
 
-    public StudentDTO loginStudent(String username, String password) throws SQLException {
+    public UserDTO loginStudent(String username, String password) throws SQLException {
         ResultSet resultSet = null;
 
         StudentDTO user = null;
@@ -68,9 +68,9 @@ public class ServiceImpl implements Service {
 
         while (resultSet.next()) {
             user = new StudentDTO();
-            user.setuser(resultSet.getString("cbs_mail"));
-            user.setpassword(resultSet.getString("password"));
-            user.settype(resultSet.getString("type"));
+            user.setMail(resultSet.getString("cbs_mail"));
+            user.setPassword(resultSet.getString("password"));
+            user.setType(resultSet.getString("type"));
         }
     }catch(SQLException e)
 
@@ -89,42 +89,15 @@ return user;
 
     }
 
-    public LectureDTO getLecture(String placeholder, String placeholder2) throws SQLException{
-        ResultSet resultSet = null;
-        LectureDTO lecture = null;
-
-        try {
-            PreparedStatement Lecture = dbConnection.prepareStatement("SELECT * FROM placeholder where placeholder = ?");
-
-            Lecture.setString(1, placeholder);
-            Lecture.setString(2, placeholder2);
-
-            resultSet = Lecture.executeQuery();
-
-            while (resultSet.next()) {
-                lecture = new LectureDTO();
-                lecture.setplaceholder(resultSet.getString("placeholder"));
-                lecture.setplaceholder2(resultSet.getString("placeholder2"));
-
-
-            }
-        }
-
-    return lecture;
-    }
-
-
-
-
-    public boolean addReview (StudentDTO user) throws IllegalArgumentException {
+    public boolean addReview (ReviewDTO review) throws IllegalArgumentException {
 
         try {
             //Prepared statement der bruges i databasen
-            PreparedStatement addReview = connection.prepareStatement(
+            PreparedStatement addReview = dbConnection.prepareStatement(
                     "INSERT INTO review (rating, comment) VALUES (?, ?)");
 
-            addReview.setString(1, user.getRating());
-            addReview.setString(2, user.getComment());
+            addReview.setInt(1, review.getRating());
+            addReview.setString(2, review.getComment());
 
             int rowsAffected = addReview.executeUpdate();
             if (rowsAffected == 1) {
@@ -135,31 +108,7 @@ return user;
         }
         return false;
     }
-
-
-    public boolean addReview (StudentDTO user) throws IllegalArgumentException {
-
-        try {
-            //Prepared statement der bruges i databasen
-            PreparedStatement addReview = connection.prepareStatement(
-                    "INSERT INTO review (rating, comment) VALUES (?, ?)");
-
-            addReview.setString(1, user.getRating());
-            addReview.setString(2, user.getComment());
-
-            int rowsAffected = addReview.executeUpdate();
-            if (rowsAffected == 1) {
-                return true;
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
-
-
+    
 
 }
 
-**/
