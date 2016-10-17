34147d7064ab2681c02288228f64060ba9b0dfbf
Gerishanth <geri.sivakumar@live.com>
2016-10-17 13:29:09 +0200

    MainCtrl updated, skriver ikke login fejl.

--- src/logic/AdminController.java
+++ src/logic/AdminController.java
@@ -5,11 +5,14 @@ import dal.ServiceImpl;
 /**
  * Created by emilstepanian on 12/10/2016.
  */
-public class AdminController {
+public class AdminController extends UserController {
 
-    public static void main (String[] args) {
-        ServiceImpl serviceImpl = new ServiceImpl();
-
-        new MainController(serviceImpl);
+    public AdminController () {
+        super();
     }
+
+    public void deleteComment() {
+
+    }
+
 }
