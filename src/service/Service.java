76f7dd65101369d162cc1efcbf37f25c7b81b90e
Mathias Lund <mathias@graduateland.com>
2016-10-17 16:02:07 +0200

    updated services

--- src/service/Service.java
+++ src/service/Service.java
@@ -1,5 +1,6 @@
 package service;
 
+import com.sun.org.apache.xpath.internal.operations.String;
 import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
 import shared.CourseDTO;
 import shared.LectureDTO;
@@ -12,14 +13,12 @@ import java.util.ArrayList;
 public interface Service {
 
     public UserDTO login(String username, String password);
-    public boolean addReview(ReviewDTO review);
+    public boolean addReview(Integer rating, String comment);
     public CourseDTO insertCourses(CourseDTO courses);
     public ArrayList<CourseDTO> getCourses(UserDTO user);
-    public boolean insertReview(ReviewDTO review);
     public ArrayList<ReviewDTO> getReviews(LectureDTO lecture);
     public boolean deleteReview(ReviewDTO review);
     public boolean deleteReviewComment(ReviewDTO review);
     public ArrayList<LectureDTO> getLectures(UserDTO users);
-
 
 }
