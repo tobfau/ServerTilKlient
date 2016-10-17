65036866c5977762364adf3bce8ef0ddcad03071
Mathias Lund <mathias@graduateland.com>
2016-10-17 15:36:43 +0200

    updated service

--- src/service/Service.java
+++ src/service/Service.java
@@ -1,7 +1,25 @@
 package service;
 
-/**
- * Created by emilstepanian on 12/10/2016.
- */
+import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
+import shared.CourseDTO;
+import shared.LectureDTO;
+import shared.ReviewDTO;
+import shared.UserDTO;
+
+import java.util.ArrayList;
+
+
 public interface Service {
+
+    public UserDTO login(String username, String password);
+    public boolean addReview(ReviewDTO review);
+    public CourseDTO insertCourses(CourseDTO courses);
+    public ArrayList<CourseDTO> getCourses(UserDTO user);
+    public boolean insertReview(ReviewDTO review);
+    public ArrayList<ReviewDTO> getReviews(LectureDTO lecture);
+    public boolean deleteReview(ReviewDTO review);
+    public boolean deleteReviewComment(ReviewDTO review);
+    public ArrayList<LectureDTO> getLectures(UserDTO users);
+
+
 }
