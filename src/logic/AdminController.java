3313581ba0e069a46bd9663026b9782a264d1571
gerish <gerish@users.noreply.github.com>
2016-10-17 15:30:26 +0200

    TUI er oprettet, for admin log ind og menu. MainControlleren og Admin controlleren styrer TUI.

--- src/logic/AdminController.java
+++ src/logic/AdminController.java
@@ -1,18 +1,116 @@
 package logic;
 
 import dal.ServiceImpl;
+import service.Service;
+import shared.AdminDTO;
+import shared.StudentDTO;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
  * Created by emilstepanian on 12/10/2016.
  */
 public class AdminController extends UserController {
 
+    //currentUser
+
     public AdminController () {
-        super();
+        super(service);
     }
+
+    public void Menu(AdminDTO adminDTO){
+        Scanner input = new Scanner(System.in);
+        try{
+
+            System.out.println("Velkommen til Undervisningsevaluering for CBS studerende og professorer!");
+            System.out.println("Du er logget ind som Admin" /* brugernavn?*/);
+            System.out.println("Tast 0 for at stoppe programmet og log ud.");
+            System.out.println("Tast 1 for at oprette en ny bruger. ");
+            System.out.println("Tast 2 for slet en bruger. ");
+            System.out.println("Tast 3 for slet en kommentar");
+
+            int choice = input.nextInt();
+
+            switch (choice) {
+                case 0:
+                    System.out.println("Programmet er stoppet og du er logget ud.");
+                    break;
+
+                case 1:
+                    createUser();
+                    break;
+
+                case 2:
+                    deleteUser();
+                    break;
+
+                case 3:
+                    deleteComment();
+                    break;
+
+                default:
+                    System.out.println("Du indtastede en forkert vaerdi, proev igen.\n");
+            }
+
+	/*Her er en catch som tr�der frem, hvis brugeren taster en forkert vaerdi. */
+        }catch(InputMismatchException e){
+            System.out.printf("Systemet fandt fejlen: %s \n",e);
+            System.out.println("Du indtastede ikke et heltal, menuen kører forfra");
+            input.nextLine();
+        }
+    }
+
 
     public void deleteComment() {
 
+       for (int i : i==loadLectures(); : i++){
+            //int idLectures =
+            System.out.println(idLectures + loadLectures());
+        }
     }
 
+    public void deleteUser(StudentDTO student) {
+        // ShowAllUsers
+
+        // ServiceImpl.
+        //int userIdFromDb = ServiceImpl.
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Indtast id på bruger der skal slettes: ");
+
+        String userId = input.nextLine();
+        //det gemte brugernavn skal sendes til Service
+
+        if (userId.equals(userIdFromDb)) {
+            System.out.println("Brugeren " + ServiceImpl. + " er nu fjernet. \n");
+        } else {
+            System.out.println("Forkerte værdier er indtastet.");
+            System.out.println("Tast 0 for at stoppe programmet.");
+            System.out.println("Tast 1 for at prøve igen.");
+            System.out.println("Tast 2 for at gå tilbage til menuen.");
+
+
+            int choice = input.nextInt();
+
+            switch (choice) {
+                case 0:
+                    System.out.println("Programmet er stoppet.");
+                    break;
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                default:
+                    System.out.println("Du indtastede en forkert vaerdi, proev igen.\n");
+            }
+        }
+    }
+
+    public void createUser(){
+
+    }
 }
--- src/logic/MainController.java
+++ src/logic/MainController.java
@@ -2,12 +2,17 @@ package logic;
 
 import dal.ServiceImpl;
 import security.Digester;
+import shared.AdminDTO;
+import shared.StudentDTO;
+import shared.TeacherDTO;
 import shared.UserDTO;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.sql.SQLException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
  * Created by emilstepanian on 12/10/2016.
@@ -28,10 +33,7 @@ public class MainController {
         studentCtrl = new StudentController();
     }
 
-    public void login () {
-
-        String mail = "as";
-        String password = "123456";
+    public void login (String mail, String password) {
 
         //hashing
         String securedPassword = Digester.hashWithSalt(password);
@@ -42,17 +44,26 @@ public class MainController {
         } catch (SQLException e){
             System.out.print(e.getMessage());
         }
-            if (user.getType().equals("admin")) {
-                adminCtrl = new AdminController();
-                //adminCtrl.loadAdmin(user);
-            }
             if (user.getType().equals("teacher")) {
                 teacherCtrl = new TeacherController();
                 //teacherCtrl.loadTeacher(user);
+
+                TeacherDTO teacherDTO = new TeacherDTO();
+                teacherDTO.setCbsMail(mail);
+                teacherDTO.setPassword(securedPassword);
+
+                //teacherCtrl.
+
             }
             if (user.getType().equals("student")) {
                 studentCtrl = new StudentController();
                 //studentCtrl.loadStudent(user);
+
+                StudentDTO studentDTO = new StudentDTO();
+                studentDTO.setCbsMail(mail);
+                studentDTO.setPassword(securedPassword);
+
+                //studentCtrl.
             }
     }
       /*  //else træder i kraft ved forkert mail eller password, der er her mulighed for et output til klienten om fejl ved login.
@@ -60,4 +71,64 @@ public class MainController {
             System.out.println("Forkert log in!");
             //DataOutputStream OutToClient = new DataOutputStream();
         }*/
+
+      public void TUILogIn(){
+          Scanner input = new Scanner(System.in);
+          try{
+          System.out.println("Velkommen til Undervisningsevaluering for CBS studerende og professorer!");
+          System.out.println("Tast 0 for at stoppe programmet");
+          System.out.println("Tast 1 for og Logge ind som admin");
+
+          int choice = input.nextInt();
+
+          switch (choice) {
+              case 0:
+                  System.out.println("Programmet er stoppet og du er logget ud.");
+                  break;
+
+              case 1:
+                  System.out.println("Indtast mail:");
+                  String mail = input.nextLine();
+
+                  System.out.println("Indtast password:");
+                  String password = input.nextLine();
+
+                  //hashing første gang.
+                  String securedPassword = Digester.hashWithSalt(password);
+
+                  //hashing anden gang.
+                  String securedPassword1 = Digester.hashWithSalt(securedPassword);
+
+                  try {
+                      //Kommunikation med databasen, serviceImpl klassen bliver kaldt og metoden loginStudent køres.
+                      user = serviceImpl.loginStudent(mail, securedPassword1);
+                  } catch (SQLException e){
+                      System.out.print(e.getMessage());
+                  }
+                  if (user.getType().equals("admin")) {
+                      adminCtrl = new AdminController();
+                      //adminCtrl.loadAdmin(user);
+
+                      AdminDTO adminDTO = new AdminDTO();
+                      adminDTO.setCbsMail(mail);
+                      adminDTO.setPassword(securedPassword1);
+
+                      adminCtrl.Menu(adminDTO);
+                  }
+                  else{
+                      System.out.println("Du indtastede en forkert vaerdi, proev igen. \n");
+                      TUILogIn();
+                  }
+                  break;
+
+              default:
+                  System.out.println("Du indtastede en forkert vaerdi, proev igen.\n");
+          }
+          /*Her er en catch som tr�der frem, hvis brugeren taster en forkert vaerdi. */
+          }catch(InputMismatchException e){
+              System.out.printf("Systemet fandt fejlen: %s \n",e);
+              System.out.println("Du indtastede ikke et heltal, menuen kører forfra");
+              input.nextLine();
+          }
+    }
 }
