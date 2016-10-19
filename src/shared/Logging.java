05eafb69c85e8862931faef26aab8e8d5a6d10c9
Esben Bak <esbenbak@esbens-air.hhk.dk>
2016-10-19 11:45:19 +0200

    Logging ordnet, 11.44

--- src/shared/Logging.java
+++ src/shared/Logging.java
@@ -1,68 +1,70 @@
-package shared;
+package src.shared;
 
-import java.util.logging.ConsoleHandler;
+import java.io.IOException;
+import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.logging.FileHandler;
-import java.io.IOException;
-import java.util.logging.Handler;
-
-
-
 
 /**
- * Created by benjikyster on 17/10/2016.
+ * Created by esbenbak on 19/10/2016.
  */
-
-public static void main(String[] args){
 public class Logging {
-    public Logger log;
 
+    protected static final Logger logger = Logger.getLogger("logging");
 
-    //public Logging() {
-
-        //Logger objekt, hvor den kalder getlogger metoden
-        /*log = new Logger.getLogger("uva");
-
-        log.log(Level.FINEST, "Mindre betydelig fejl"); // 1. niveau printes ikke ud i konsol, men ses i compiler
-        log.log(Level.FINE, "Fejl"); //2. niveau
-        log.log(Level.WARNING, "Stor fejl"); //3. niveau af fejl
-        log.log(Level.SEVERE, "Største fejl"); //4. niveau
-
-        //log.log(Level.FINE,""); Oprettet af Henrik
-        fh = new FileHandler("./syslog");*/
-
-
-//Constructor
-
-        Handler consoleHandler = null;
-        Handler fileHandler = null;
+    /**
+     * Ved hjælp af switch er der
+     * @param ex
+     * @param level
+     * @param msg
+     */
+    public static void log(Exception ex, String level, String msg) {
+        FileHandler fh = null;
 
         try {
+            fh = new FileHandler("log.txt", true);
+            logger.addHandler(fh);
+            switch (level) {
 
-            //Konstruerer FileHandler
-            consoleHandler = new ConsoleHandler();
-            fileHandler = new FileHandler("./syslog.formatter.log");
+                case "FINEST":
+                    logger.log(Level.FINEST, msg, ex);
+                    if (!msg.equals(""))
+                        System.out.println(msg + "finest error");
+                    break;
+                case "FINE":
+                    logger.log(Level.FINE, msg, ex);
+                    if (!msg.equals(""))
+                        System.out.println(msg + "fine error");
+                    break;
+                case "SEVERE":
+                    logger.log(Level.SEVERE, msg, ex);
+                    if (!msg.equals(""))
+                        System.out.println(msg + "severe error");
+                    break;
+                default:
+                    logger.log(Level.CONFIG, msg, ex);
+                    break;
+
+            }
 
 
-            log.addHandler(fileHandler);
-            log.addHandler(consoleHandler);
-
-            consoleHandler.setLevel(Level.ALL);
-            fileHandler.setLevel(Level.ALL);
-            log.setLevel(Level.ALL);
-
-            log.config("konfiguration lavet");
-
-            log.removeHandler(consoleHandler);
-
-            log.log(Level.FINE, "Fejl logget");
-
-        } catch(IOException exception) {
-
-            log.log(Level.SEVERE, "Fejl i FileHandler!", exception);
+        } catch (IOException | SecurityException ex1) {
+            logger.log(Level.SEVERE, null, ex1);
+        } finally {
+            if (fh != null) fh.close();
         }
+    }
 
+    public static void main(String[] args) {
+        IOException e = new IOException();
+        try {
+            throw e;
+        } catch (IOException e1) {
+            e1.printStackTrace();
+
+        }
+        log(e, "SEVERE", "hey");
     }
 
 }
+
