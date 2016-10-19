d5fc1bad4491a2ecfdc745abac851bbfd2ad09c3
Henrik Thorn <henrikthorn@it-kartellet.dk>
2016-10-19 17:07:35 +0200

    Fixed entire project and now commit the worst message ever

--- src/Run.java
+++ src/Run.java
@@ -10,30 +10,7 @@ import javax.ws.rs.*;
 import java.io.PrintStream;
 
 //TODO: Missing documentation and use of config variables.
-
-@Path("/api")
 public class Run {
-
-    @GET
-    @Path("/getLectures/{courseId}")
-    public String getLectures(@PathParam("courseId") int courseId) {
-        Gson gson = new Gson();
-
-        UserController ctrl = new UserController();
-
-        return gson.toJson(ctrl.getLectures(courseId));
-    }
-
-    @GET
-    @Path("/getCourses/{userId}")
-    public String getCourses(@PathParam("userId") int userId) {
-        Gson gson = new Gson();
-
-        UserController ctrl = new UserController();
-        return gson.toJson(ctrl.getCourses(userId));
-
-    }
-
 
     public static void main(String[] args) throws IOException {
 
@@ -58,21 +35,6 @@ public class Run {
         Logging.initiateLog(ConfigLoader.DEBUG);
 
         //Loader courses og lectures ind til databasen
-        System.out.println("Server henter fag og lektioner.");
-        System.out.println("Det kan tage op til 40 sekunder...");
-
-        //Parse CBS data til database
-        /**try {
-            CBSParser.parseCBSData();
-        } catch (SQLException e){
-            e.printStackTrace();
-        }*/
-
-
-        //Starter MainController
-        //Service service = new ServiceImpl();
-        //new = MainController(service);
-
         System.out.println("Server running");
         System.out.println("Visit: http://" + ConfigLoader.SERVER_ADDRESS + ":" + ConfigLoader.SERVER_PORT + "/");
         System.out.println("Hit return to stop...");
