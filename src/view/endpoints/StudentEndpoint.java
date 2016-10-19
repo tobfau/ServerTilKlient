899993d846c7daf183a98479b5cc58a11d8c8d68
gittinjiggywithit <kasperfriis93@gmail.com>
2016-10-19 12:44:54 +0200

    - startede på createReview

--- src/view/endpoints/StudentEndpoint.java
+++ src/view/endpoints/StudentEndpoint.java
@@ -1,9 +1,19 @@
 package view.endpoints;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
 /**
  * Created by Kasper on 19/10/2016.
  */
 public class StudentEndpoint extends UserEndpoint {
+
+    @POST
+    @Path("/getLectures/{courseId}")
+    public static void createReview(String json){
+
+    }
 
 
 }
