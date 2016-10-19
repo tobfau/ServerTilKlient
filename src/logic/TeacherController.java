c4a2bb9fadb0723e84aa22e6963a68053bab5a11
Gerishanth <geri.sivakumar@live.com>
2016-10-19 09:06:09 +0200

    TeacherCtrl updated.

--- src/logic/TeacherController.java
+++ src/logic/TeacherController.java
@@ -1,10 +1,33 @@
 package logic;
 
+import service.Service;
+import shared.TeacherDTO;
+
 /**
  * Created by emilstepanian on 12/10/2016.
  */
 public class TeacherController {
 
+    private TeacherDTO currentUser;
+    private Service service;
 
 
+    // laver gennemsnittet af ratings
+    public double average() {
+        /*
+        Review arraylist skal hentes og derfra kun trækkes ratings.
+         */
+        int sum = 0;
+        double average;
+
+        //Hver rating bliver lagt sammen
+        for(int i=0; i < array.length; i++){
+            sum = sum + array[i];
+        }
+
+        //samlet rating bliver divideret med antallet af ratings
+        average = (double) sum/array.length;
+        //gennemsnittet bliver returneret
+        return average;
+    }
 }
