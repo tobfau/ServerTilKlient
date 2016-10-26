3a0043eced33ab0ba6727e8117d0df1c2a044675
Elena Bundgaard Noltensmier <Elena@elenas-mbp-2.hhk.dk>
2016-10-26 12:20:16 +0200

    Kommentarer til ConfigLoader

--- src/logic/ConfigLoader.java
+++ src/logic/ConfigLoader.java
@@ -1,5 +1,4 @@
 package logic;
-//TODO: Dokumentation needed for this class!
 
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
@@ -13,10 +12,12 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
+/*ConfigLoader anvendes til at hente den unikke config.Json fil, vi har uploadet, hvilket gør f.eks database adgangen uafhægig af hvilken computer den ligger på*/
 public class ConfigLoader {
 
-    /* Static variable indlæses alle som String. ServerPort skal egentlig være en int, men det må løses med en
-       Integer.parseInt() metode, hvor den skal bruges.
+    /* Static variable indlæses alle som String, fordi de hentes fra config.Json.
+    OBS! ServerPort skal anvendes som en int, og dette løses med en
+       Integer.parseInt(), der hvor ServerPorten skal anvendes.
      */
     public static String DB_TYPE;
     public static String DB_HOST;
@@ -34,36 +35,41 @@ public class ConfigLoader {
     public static String DEBUG;
     public static String ENCRYPTION;
 
+    /* Der oprettes en static final kombineret med SINGLETON(design mønster), som har til formål,
+    at kontrollere intialiseringen af klassen for derved at sørge for at objektet ikke kan instansieres mere end en gang */
+
     private static final ConfigLoader SINGLETON = new ConfigLoader();
 
-    public ConfigLoader getInstance(){
+    public ConfigLoader getInstance() {
         return SINGLETON;
     }
 
-    private ConfigLoader(){
+    /* Når metoden ConfigLoader køres, startes parseConfig()*/
+    private ConfigLoader() {
         parseConfig();
     }
 
+    /* parseConfig retunerer samtlige værdier fra den lokale Config.Json-fil*/
     public static void parseConfig() {
         JsonParser jparser = new JsonParser();
         JsonReader jsonReader;
 
         try {
-            jsonReader = new JsonReader(new FileReader("config.json"));
-            JsonObject jsonObject = jparser.parse(jsonReader).getAsJsonObject();
+            jsonReader = new JsonReader(new FileReader("config.json")); //opretter metode til at læse den lokale config.Json-fil.
+            JsonObject jsonObject = jparser.parse(jsonReader).getAsJsonObject(); //opretter et objekt af config.Json-filen.
 
-            Set<Map.Entry<String, JsonElement>> entries = jsonObject.entrySet();//will return members of your object
+            Set<Map.Entry<String, JsonElement>> entries = jsonObject.entrySet();//retunerer værdierne af objektet
 
-            for (Map.Entry<String, JsonElement> entry: entries) {
+            for (Map.Entry<String, JsonElement> entry : entries) {
                 try {
-                    ConfigLoader.class.getDeclaredField(entry.getKey()).set(SINGLETON, entry.getValue().getAsString());
+                    ConfigLoader.class.getDeclaredField(entry.getKey()).set(SINGLETON, entry.getValue().getAsString());//sætter static værdierne som strings
 
-                } catch (Exception e){
+                } catch (Exception e) {
                     System.out.println(e.getMessage());
                 }
             }
-            
-        } catch (Exception e){
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
